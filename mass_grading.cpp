68ea71b92c576dd1b5e9ba7d979e98f718653dca
ChaiteAlex <136808818+ChaiteAlex@users.noreply.github.com>
2026-03-30 12:04:04 +0500

    Create mass_grading.cpp

---
+++ mass_grading.cpp
@@ -0,0 +1,35 @@
+/* Вариант 14
+В массиве представлены данные об n средних чеках покупателей в магазине.
+Найти все аномальные выбросы в этом массиве, используя формулу Z-оценки.
+*/
+
+int main()
+{
+	using namespace std;
+
+	const int n = 10;
+	
+	double mas [n];
+	double arf_mean = 0, sum = 0, sigma, zi;
+	
+	double a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		mas[i] = a;
+		arf_mean += a;
+	}
+	arf_mean /= n;
+	
+	for (int i = 0; i < n; i++) {
+		sum += pow((mas[i] - arf_mean), 2);
+	}
+
+	sigma = pow((sum / 9), 0.5); //отклонение от
+	
+	printf_s("mean: %.4f diff: %.4f\n", arf_mean, sigma);
+
+	for (int i = 0; i < n; i++) {
+		zi = (mas[i] - arf_mean) / sigma;
+		printf_s("%i element - %.4f dif: %.4lf \n\n", i, mas[i], zi);
+	}
+}	
